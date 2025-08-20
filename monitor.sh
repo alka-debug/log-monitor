822a9eeb7a29eaf7a9c493223dcb353e4df8f35f
alka-debug <kohlialka22@gmail.com>
2025-08-20 15:52:04 +0530

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-# define which log to monitor
 LOGFILE="/var/log/syslog"
+SIZE=$(du -m $LOGFILE | cut -f1)
+SIZE=$((SIZE+0))  # ensure integer
 
-# check its size
-SIZE=$(du -h $LOGFILE | cut -f1)
-#SIZE=$((SIZE+0)) # ensure integer
-
-BACKUP_DIR="/home/cupcake/log-monitor/backups"
+BACKUP_DIR="/home/alka/log-monitor/backups"
 
 # make backup dir if not exist
 mkdir -p $BACKUP_DIR
 
-# conditional check
+# Conditional check
 if [ $SIZE -gt 1 ]; then
     TIMESTAMP=$(date +%F-%H%M)
-    BACKUP_FILE="$BACKUP_FILE/syslog-$TIMESTAMP.log"
-    cpp $LOGILE $BACKUP_FILE # copy log to backup
-  echo "WARNING: $LOGFILE is too big ($SIZE ), backup saved as $BACKUP_FILE"
+    BACKUP_FILE="$BACKUP_DIR/syslog-$TIMESTAMP.log"
+    cp $LOGFILE $BACKUP_FILE       # copy log to backup
+    echo "WARNING: $LOGFILE is too big ($SIZE MB), backup saved as $BACKUP_FILE"
 else
-  echo "Log size is chill:) ($SIZE )"
+    echo "Log size OK ($SIZE MB)"
 fi
