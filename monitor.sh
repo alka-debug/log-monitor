ab07b03a4a97c957924178ee9b482ae165d6f6cb
Alka <kohlialka22@gmail.com>
2025-08-20 15:41:30 +0530

    daily backups

--- monitor.sh
+++ monitor.sh
@@ -5,11 +5,19 @@ LOGFILE="/var/log/syslog"
 
 # check its size
 SIZE=$(du -m $LOGFILE | cut -f1)
-SIZE=$((SIZE+0))
+SIZE=$((SIZE+0)) # ensure integer
+
+BACKUP_DIR="/home/cupcake/log-monitor/backups"
+
+# make backup dir if not exist
+mkdir -p $BACKUP_DIR
 
 # conditional check
 if [ $SIZE -gt 1 ]; then
-  echo "WARNING: $LOGFILE is too big ($SIZE mb)"
+    TIMESTAMP=$(date +%F-%H%M)
+    BACKUP_FILE="$BACKUP_FILE/syslog-$TIMESTAMP.log"
+    cpp $LOGILE $BACKUP_FILE # copy log to backup
+  echo "WARNING: $LOGFILE is too big ($SIZE mb), backup saved as $BACKUP_FILE"
 else
   echo "Log size is chill:) ($SIZE mb)"
 fi
