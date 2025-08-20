2d78b2377460d105c5f5e4be159d046308728b44
Alka <kohlialka22@gmail.com>
2025-08-20 15:04:14 +0530

    add size check in mb, fix integer error

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-#define which log to monitor
+
+# define which log to monitor
 LOGFILE="/var/log/syslog"
-#check its size
-SIZE=$(du -h $LOGFILE | cut -f1)
-#print a message
-echo "The size of $LOGFILE is $SIZE"
+
+# check its size
+SIZE=$(du -m $LOGFILE | cut -f1)
+SIZE=$((SIZE+0))
+
+# conditional check
+if [ $SIZE -gt 1 ]; then
+  echo "WARNING: $LOGFILE is too big ($SIZE mb)"
+else
+  echo "Log size is chill:) ($SIZE mb)"
+fi
